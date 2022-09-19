802ccdbfbbfecf0d3f364ea43191171ba7fed123
unknown <ademayowaadeniyi@gmail.com>
2022-09-19 09:44:13 +0100

    added invest component

---
+++ components/Invest.js
@@ -0,0 +1,40 @@
+import Image from 'next/image';
+
+export default function Invest() {
+  return (
+    <section className='relative bg-[#153048] px-5 md:px-12 mt-16 md:mt-20 pb-20 rounded-lg lg:mx-20 min-h-max'>
+      <div className='wrapper flex flex-col lg:flex-row justify-between pt-10'>
+        <div className='flex flex-col w-full lg:w-1/4'>
+          <h3 className='text-sm text-[#FFA72A]'>Are you LOOKING TO INVEST?</h3>
+          <p className='text-[27px] text-white max-w-md'>
+            We have tokenized real estate assets you can invest in
+          </p>
+        </div>
+
+        <div className='flex flex-col w-full lg:w-2/5 mt-5 lg:mt-0'>
+          <p className='text-white max-w-sm text-[15px]'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis, pulvinar sed do eiusmod
+            tempor.
+          </p>
+
+          <div className='mt-5'>
+            <button className='bg-[#EC3335] text-white inline-block px-6 py-2.5 border-red-600 font-medium text-[15px] leading-tight rounded hover:bg-black hover:bg-opacity-5 border focus:outline-none focus:ring-0 transition duration-150 ease-in-out'>
+              Signup as an Investor
+            </button>
+          </div>
+        </div>
+      </div>
+
+      <div className='hidden lg:block absolute -top-32 -right-20'>
+        <Image
+          width={270}
+          height={470}
+          objectFit='contain'
+          src={'/woman.png'}
+          alt='woman-img'
+        />
+      </div>
+    </section>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero';
+import Invest from '@/components/Invest';
 import Layout from '@/components/Layout';
 import Properties from '@/components/Properties';
 
@@ -7,6 +8,7 @@ export default function HomePage() {
     <Layout title='Reits Africa | Home'>
       <Hero />
       <Properties />
+      <Invest />
     </Layout>
   );
 }
