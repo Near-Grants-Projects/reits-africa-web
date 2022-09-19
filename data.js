a16cbebb2729b66917615c5d2a35397c1eb868f5
unknown <ademayowaadeniyi@gmail.com>
2022-09-19 07:17:40 +0100

    added dummy data

---
+++ data.js
@@ -0,0 +1,26 @@
+export const images = [
+  {
+    id: 1,
+    img: 'https://res.cloudinary.com/dpxs9ause/image/upload/v1647861754/reits-africa/house_m5jr0k.png',
+  },
+  {
+    id: 2,
+    img: 'https://res.cloudinary.com/dpxs9ause/image/upload/v1647861754/reits-africa/house_m5jr0k.png',
+  },
+  {
+    id: 3,
+    img: 'https://res.cloudinary.com/dpxs9ause/image/upload/v1647861754/reits-africa/house_m5jr0k.png',
+  },
+  {
+    id: 4,
+    img: 'https://res.cloudinary.com/dpxs9ause/image/upload/v1647861754/reits-africa/house_m5jr0k.png',
+  },
+  {
+    id: 5,
+    img: 'https://res.cloudinary.com/dpxs9ause/image/upload/v1647861754/reits-africa/house_m5jr0k.png',
+  },
+  {
+    id: 6,
+    img: 'https://res.cloudinary.com/dpxs9ause/image/upload/v1647861754/reits-africa/house_m5jr0k.png',
+  },
+];
